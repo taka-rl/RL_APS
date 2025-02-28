77bb273a954adb490f230056c6ded217a9e0ec4f
taka_rl <takaya.murakami.uni@gmail.com>
2025-03-01 00:07:29 +0100

    updated test_parking_env.py

--- tests/test_parking_env.py
+++ tests/test_parking_env.py
@@ -4,6 +4,7 @@ from gymnasium.spaces import Discrete
 
 from sim_env.parking_env import Parking
 from sim_env.parameters import Config
+from sim_env.car import Car
 
 
 # --------------------------------------------- Common functions for test ---------------------------------------------
@@ -54,6 +55,16 @@ def discrete_parallel_env():
     }
     return Parking(env_config)
 
+
+def parking_env(reward_type: str = 'type1', state_type: str = 'type1'):
+    env_config = {
+        "render_mode": "no_render",
+        "action_type": "continuous",
+        "parking_type": "perpendicular",
+        "training_mode": "off",
+        "config": Config(reward_type=reward_type, state_type=state_type)
+    }
+    return Parking(env_config)
 
 # --------------------------------------------- Initialization ---------------------------------------------
 def test_continue_perpendicular_env_init(continuous_perpendicular_env):
@@ -106,14 +117,14 @@ def test_discrete_perpendicular_env_step(discrete_perpendicular_env):
     # Reset the environment
     discrete_perpendicular_env.reset()
     assert isinstance(discrete_perpendicular_env.action_space, Discrete)
-    assert discrete_perpendicular_env.action_space.n == 6, "Action space should have 6 discrete actions."
+    assert discrete_perpendicular_env.action_space.n == 6, "Action space shall have 6 discrete actions."
 
     # Valid actions (0 to 5)
     actions = (0, 1, 2, 3, 4, 5)
     for action in actions:
         state, reward, terminated, truncated, info = discrete_perpendicular_env.step(action)
         assert state is not None, "State shall update after a step."
-        assert isinstance(state, np.ndarray), "State should be a NumPy array."
+        assert isinstance(state, np.ndarray), "State shall be a NumPy array."
         assert np.all(state >= -1.0) and np.all(state <= 1.0), "State value shall be between -1.0 and 1.0"
         assert isinstance(reward, (int, float)), "Reward shall be a number."
         assert isinstance(terminated, bool), "Terminated flag shall be a boolean."
@@ -129,6 +140,8 @@ def test_continue_perpendicular_env_reset(continuous_perpendicular_env):
     """Test reset functionality."""
     state, _ = continuous_perpendicular_env.reset()
     assert state is not None, "Reset shall return an initial state."
+    assert isinstance(state, np.ndarray), "State shall be a NumPy array."
+    assert np.all(state >= -1.0) and np.all(state <= 1.0), "State value shall be between -1.0 and 1.0"
     assert not continuous_perpendicular_env.terminated, "Environment shall not be terminated after reset."
     assert not continuous_perpendicular_env.truncated, "Environment shall not be truncated after reset."
 
@@ -136,3 +149,55 @@ def test_continue_perpendicular_env_reset(continuous_perpendicular_env):
 # --------------------------------------------- Reward ---------------------------------------------
 
 # --------------------------------------------- State ---------------------------------------------
+def test_state_type1():
+    """Test if the state type1 is returned properly."""
+    env = parking_env(state_type='type1')
+    assert env.config.state_type == 'type1'
+
+    env.reset()
+
+    env.car = Car(([20, 20]), 0, Config())
+
+    action = [1.0, 1.0]
+    state, reward, terminated, truncated, info = env.step(action)
+
+    assert state is not None, "State shall update after a step."
+    assert isinstance(state, np.ndarray), "State shall be a NumPy array."
+    assert np.all(state >= -1.0) and np.all(state <= 1.0), "State value shall be between -1.0 and 1.0"
+    assert state.shape is not 8, "State shape shall be 8."
+
+
+def test_state_type2():
+    """Test if the state type2 is returned properly."""
+    env = parking_env(state_type='type2')
+    assert env.config.state_type == 'type2'
+
+    env.reset()
+
+    env.car = Car(([20, 20]), 0, Config())
+
+    action = [1.0, 1.0]
+    state, reward, terminated, truncated, info = env.step(action)
+
+    assert state is not None, "State shall update after a step."
+    assert isinstance(state, np.ndarray), "State shall be a NumPy array."
+    assert np.all(state >= -1.0) and np.all(state <= 1.0), "State value shall be between -1.0 and 1.0"
+    assert state.shape is not 10, "State shape shall be 10."
+
+
+def test_state_type3():
+    """Test if the state type3 is returned properly."""
+    env = parking_env(state_type='type3')
+    assert env.config.state_type == 'type3'
+
+    env.car = Car(([20, 20]), 0, Config())
+
+    env.reset()
+
+    action = [1.0, 1.0]
+    state, reward, terminated, truncated, info = env.step(action)
+
+    assert state is not None, "State shall update after a step."
+    assert isinstance(state, np.ndarray), "State shall be a NumPy array."
+    assert np.all(state >= -1.0) and np.all(state <= 1.0), "State value shall be between -1.0 and 1.0"
+    assert state.shape is not 9, "State shape shall be 9."
