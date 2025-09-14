c46be62899ecf6ba149d72f3022d7b688cae7114
Takaya <157423802+taka-rl@users.noreply.github.com>
2025-09-15 01:11:29 +0200

    Revert "22 fix get normalized state function"

--- sim_env/parking_env.py
+++ sim_env/parking_env.py
@@ -258,7 +258,7 @@ class Parking(gym.Env):
         distances = np.array(distances).flatten()
 
         # normalization
-        normalized_distances = distances / self.config.max_distance
+        normalized_distances = distances / self.config.velocity_limit
 
         # type1 state (default state)
         if self.config.state_type == 'type1':
